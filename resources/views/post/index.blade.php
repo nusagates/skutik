2694bbdea6022ced14cbe47ba3355e6426bd6aef
CakBud <budairi.contact@gmail.com>
2020-09-01 20:14:27 +0700

    meta

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,16 +1,4 @@
 @extends('layouts.app')
-@if($post)
-@section('meta')
-    <meta property="og:type" content="website"/>
-    <meta property="og:url" content="{{$post->url}}"/>
-    <meta property="og:site_name" content="Skutik"/>
-    <meta property="og:image" itemprop="image primaryImageOfPage" content="{{$post->featured_image}}"/>
-    <meta name="twitter:card" content="summary"/>
-    <meta name="twitter:domain" content="skutik.comcom"/>
-    <meta name="twitter:title" property="og:title" itemprop="name" content="{{$post->post_title." - ".config('app.name')}}"/>
-    <meta name="twitter:description" property="og:description" itemprop="description" content="{{$post->description}}"/>
-@endsection
-@endif
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
