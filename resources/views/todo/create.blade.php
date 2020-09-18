b494fe234401b56009fba945d50186b579313bb2
CakBud <budairi.contact@gmail.com>
2020-09-18 15:56:45 +0700

    tambah todo

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', set_title('Buat Tugas'))
+@section('title', set_title('Buat Proyek'))
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
